a0a5bf7c5482afb1342a84ff375c65c139fd9d84
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-25 22:35:58 +0700

    Create Replace multiple values II.py

---
+++ 2.Replacing values in a DataFrame/Replace multiple values II.py
@@ -0,0 +1,6 @@
+start_time = time.time()
+
+# Replace ethnicities as instructed
+names['Ethnicity'].replace(['ASIAN AND PACI','BLACK NON HISP', 'WHITE NON HISP'], ['ASIAN AND PACIFIC ISLANDER','BLACK NON HISPANIC','WHITE NON HISPANIC'], inplace=True)
+
+print("Time using .replace(): {} sec".format(time.time() - start_time))
